8846a16f36bbe4bc9afd95ba99dc588c6171512b
Nouramairy <nour.amairy.Net25@edu.edugrade.se>
2026-05-22 07:53:24 +0200

    Add employee service with in-memory storage

---
+++ EmployeeManager/Services/EmployeeService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeManager.Models;
+
+namespace EmployeeManager.Services;
+
+public class EmployeeService
+{
+    private readonly List<Employee> _employees = [];
+    private int _nextId = 1;
+
+    public int Count => _employees.Count;
+
+    public IReadOnlyList<Employee> GetAll()
+    {
+        return _employees;
+    }
+
+    public Employee Add(string firstName, string lastName, string role, string email)
+    {
+        var employee = new Employee
+        {
+            Id = _nextId,
+            FirstName = firstName,
+            LastName = lastName,
+            Role = role,
+            Email = email
+        };
+
+        _employees.Add(employee);
+        _nextId++;
+
+        return employee;
+    }
+}
